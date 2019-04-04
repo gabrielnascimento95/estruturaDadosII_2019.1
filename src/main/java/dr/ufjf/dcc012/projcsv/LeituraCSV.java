cf76e702afe2b2bfff98b9b53eafd492a208f944
Gabriel Nascimento <gabriel.nascimento@ice.ufjf.br>
2019-04-04 19:36:36 -0300

    leitura .csv

---
+++ src/main/java/dr/ufjf/dcc012/projcsv/LeituraCSV.java
@@ -0,0 +1,19 @@
+package dr.ufjf.dcc012.projcsv;
+
+/**
+ * LeituraCSV
+ */
+public class LeituraCSV {
+
+    /*
+    List<CSVUser> csvUsers = csvToBean.parse();
+
+    for(CSVUser csvUser: csvUsers) {
+    System.out.println("Name : " + csvUser.getName());
+    System.out.println("Email : " + csvUser.getEmail());
+    System.out.println("PhoneNo : " + csvUser.getPhoneNo());
+    System.out.println("Country : " + csvUser.getCountry());
+    System.out.println("==========================");
+    }
+    */
+}
