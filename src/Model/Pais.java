35b2de20b958b9196030469dcaa22d4f8e36f5d8
Gabriel Nascimento <gabriel.nascimento@ice.ufjf.br>
2019-04-25 20:24:00 -0300

    atualizacao do projeto

---
+++ src/Model/Pais.java
@@ -0,0 +1,29 @@
+package Model;
+
+public class Pais {
+
+	String cod;
+	String nome;
+	
+	public Pais(String nome, String codigo) {
+		this.cod = codigo;
+		this.nome = nome;
+	}
+	
+	public String getCod() {
+		return cod;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setCod(String cod) {
+		this.cod = cod;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
