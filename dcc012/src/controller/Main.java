b16bf29fadc139d5beaaf595a9f3783e2b417fea
Gabriel Nascimento <gabriel.nascimento@ice.ufjf.br>
2019-04-27 10:04:58 -0300

    leitura TXT

--- dcc012/src/controller/Main.java
+++ dcc012/src/controller/Main.java
@@ -1,10 +1,6 @@
 package controller;
 import java.util.ArrayList;
 import java.util.List;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import model.*;
 public class Main {
 
@@ -12,53 +8,28 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Main obj = new Main();
+		/*
+		LeituraCSV obj = new LeituraCSV();
 		List<Pais> listPais = new ArrayList<Pais>();
-		obj.leituraCSV(listPais);
+		obj.ler(listPais);
 		
 		System.out.println("\n");
 		System.out.println("---------------Objetos dentro da lista---------------");
 		for(Pais item : listPais){
 			System.out.println("Código "+ item.getCod() + " Nome: " + item.getNome());
 		}
-
+		*/
+		
+		LeituraTXT obj = new LeituraTXT();
+		List<Integer> listEntradas = new ArrayList<Integer>();
+		obj.ler(listEntradas);
+		System.out.println("\n");
+		System.out.println("---------------Objetos dentro da lista---------------");
+		for(Integer item : listEntradas){
+			System.out.println(item.intValue());
+		}
 	}
 	
-	public void leituraCSV(List listPais){
-		String arquivoCSV = "/home/gabriel/workspace/estruturaDadosII_2019.1/dcc012/arqs/arquivo.csv";
-	    BufferedReader br = null;
-	    String linha = "";
-	    String csvDivisor = ",";
-	    boolean primeiraLinha = true;
-	    
-	  
-	    try {
-	        br = new BufferedReader(new FileReader(arquivoCSV));
-	        while ((linha = br.readLine()) != null) {
-	        	if (primeiraLinha) {
-	        	    primeiraLinha = false;
-	        	    continue;
-	        	  }
-	        String[] campo = linha.split(csvDivisor);
-	        listPais.add(new Pais(campo[campo.length-1], campo[campo.length-2]));
-	        System.out.println("País [code= " + campo[campo.length-2] 
-	        									+ " , name=" + campo[campo.length-1] + "]");
-
-	        }
-
-	    }catch (FileNotFoundException e) {
-	        e.printStackTrace();
-	    }catch (IOException e) {
-	        e.printStackTrace();
-	    }finally {
-	        if (br != null) {
-	            try {
-	                br.close();
-	            } catch (IOException e) {
-	                e.printStackTrace();
-	            }
-	        }
-	    }
-	}
+	
 
 }
