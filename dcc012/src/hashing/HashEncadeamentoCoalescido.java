cbf644c05d8fa88c92a3e61f0d8a302bb3b54877
FelipeReis1 <48934779+FelipeReis1@users.noreply.github.com>
2019-05-11 19:53:15 -0300

    Update HashEncadeamentoCoalescido.java

--- dcc012/src/hashing/HashEncadeamentoCoalescido.java
+++ dcc012/src/hashing/HashEncadeamentoCoalescido.java
@@ -2,26 +2,26 @@ package hashing;
 
 public class HashEncadeamentoCoalescido {
           
-      private int key;
-      private int value;
+      private int chave;
+      private int valor;
       private HashCoalescido next;
 
-      HashCoalescido(int key, int value) {
-            this.key = key;
-            this.value = value;
+      HashCoalescido(int chave, int valor) {
+            this.chave = chave;
+            this.valor = valor;
             this.next = null;
       }
   
-      public int getValue() {
-            return value;
+      public int getValor() {
+            return valor;
       }
 
-      public void setValue(int value) {
-            this.value = value;
+      public void setValor(int valor) {
+            this.valor = valor;
       }
 
-      public int getKey() {
-            return key;
+      public int getChave() {
+            return chave;
       }
   
       public HashCoalescido getNext() {
@@ -35,60 +35,60 @@ public class HashEncadeamentoCoalescido {
 
 public class HashMap {
 
-      private final static int TABLE_SIZE = 128;
-      HashCoalescido[] table;
-      HashMap() {
-            table = new HashCoalescido[TABLE_SIZE];
-            for (int i = 0; i < TABLE_SIZE; i++)
-                  table[i] = null;
+           private final int tamanhoDaTabela;
+      HashCoalescido[] tabela;
+      HashMap(List<Integer> listEntradas) {
+            this.tamanhoDaTabela = listEntradas.size();
+            for (int i = 0; i < listEntradas.size(); i++)
+                  tabela[i] = null;
       }
 
-      public int get(int key) {
-            int hash = (key % TABLE_SIZE);
-            if (table[hash] == null)
+      public int busca(int chave) {
+            int hash = (chave % tamanhoDaTabela);
+            if (tabela[hash] == null)
                   return -1;
             else {
-                  HashCoalescido entry = table[hash];
-                  while (entry != null && entry.getKey() != key)
+                  HashCoalescido entry = tabela[hash];
+                  while (entry != null && entry.getChave() != chave)
                         entry = entry.getNext();
                   if (entry == null)
                         return -1;
                   else
-                        return entry.getValue();
+                        return entry.getValor();
             }
       }
 
-      public void put(int key, int value) {
-            int hash = (key % TABLE_SIZE);
-            if (table[hash] == null)
-                  table[hash] = new HashCoalescido(key, value);
+      public void insere(int userID, int movieID) {
+            int hash = (userID % tamanhoDaTabela);
+            if (tabela[hash] == null)
+                  tabela[hash] = new HashCoalescido(userID, movieID);
             else {
-                  HashCoalescido entry = table[hash];
-                  while (entry.getNext() != null && entry.getKey() != key)
+                  HashCoalescido entry = tabela[hash];
+                  while (entry.getNext() != null && entry.getChave() != userID)
                         entry = entry.getNext();
-                  if (entry.getKey() == key)
-                        entry.setValue(value);
+                  if (entry.getChave() == userID)
+                        entry.setValor(movieID);
                   else
-                        entry.setNext(new HashCoalescido(key, value));
+                        entry.setNext(new HashCoalescido(userID, movieID));
             }
       }
 
-      public void remove(int key) {
+      public void remove(int chave) {
 
-            int hash = (key % TABLE_SIZE);
-            if (table[hash] != null) {
+            int hash = (chave % tamanhoDaTabela);
+            if (tabela[hash] != null) {
                   HashCoalescido prevEntry = null;
-                  HashCoalescido entry = table[hash];
-                  while (entry.getNext() != null && entry.getKey() != key) {
+                  HashCoalescido entry = tabela[hash];
+                  while (entry.getNext() != null && entry.getChave() != chave) {
                         prevEntry = entry;
                         entry = entry.getNext();
                   }
-                  if (entry.getKey() == key) {
+                  if (entry.getChave() == chave) {
                         if (prevEntry == null)
-                             table[hash] = entry.getNext();
+                             tabela[hash] = entry.getNext();
                         else
                              prevEntry.setNext(entry.getNext());
                   }
             }
-      }
+    }
 }
