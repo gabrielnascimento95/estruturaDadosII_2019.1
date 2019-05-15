9905e5a110dda0363c63de206889fb039dd2da84
rafaelbrjf <31705748+rafaelbrjf@users.noreply.github.com>
2019-05-15 20:33:12 -0300

    Update QuickSortMediana.java

--- dcc012/src/algoritOrdenacao/QuickSortMediana.java
+++ dcc012/src/algoritOrdenacao/QuickSortMediana.java
@@ -2,23 +2,23 @@ package algoritOrdenacao;
 import java.util.*;
 
 public class QuickSortMediana {
-    public static void QuickSortMediana(int[] vet){        
-    	QuickSortMediana(vet, 0, vet.length - 1);
+    public static void QuickSortMediana(List<Integer> listInteiros){        
+    	QuickSortMediana(listInteiros, 0, listInteiros.size() - 1);
     }
 
-    private static void QuickSortMediana(int[] vet, int inicio, int fim){        
+    private static void QuickSortMediana(List<Integer> listInteiros, int inicio, int fim){        
         if(inicio < fim){
-            int q = particiona(vet, inicio, fim);
-            QuickSortMediana(vet, inicio, q - 1);
-            QuickSortMediana(vet, q + 1, fim);            
+            int q = particiona(listInteiros, inicio, fim);
+            QuickSortMediana(listInteiros, inicio, q - 1);
+            QuickSortMediana(listInteiros, q + 1, fim);            
         }
     }
     
-    private static int particiona(int[] vet, int inicio, int fim){
+    private static int particiona(List<Integer> listInteiros, int inicio, int fim){
         int meio = (inicio + fim)/2;
-        int a = vet[inicio];
-        int b = vet[meio];
-        int c = vet[fim];
+        int a = listInteiros.get(inicio);
+        int b = listInteiros.get(meio);
+        int c = listInteiros.get(fim);
         int medianaIndice;
         if(a < b){
             if(b < c){
@@ -41,24 +41,23 @@ public class QuickSortMediana {
                 }
             }
         }
-        troca(vet, medianaIndice, fim);
+        troca(listInteiros, medianaIndice, fim);
         
-        int pivo = vet[fim];
+        int pivo = listInteiros.get(fim);
         int i = inicio - 1;
         for(int j = inicio; j <= fim - 1; j++){
-            if(vet[j] <= pivo){
+            if(listInteiros.get(j) <= pivo){
                 i = i + 1;
-                troca(vet, i, j);
+                troca(listInteiros, i, j);
             }
         }
-        troca(vet, i + 1, fim);
+        troca(listInteiros, i + 1, fim);
         return i + 1;
     }
     
-    private static void troca(int[] vet, int i, int j){
-        int temp = vet[i];
-        vet[i] = vet[j];
-        vet[j] = temp;
+    private static void troca(List<Integer> listInteiros, int i, int j){
+        int temp = listInteiros.get(i);
+        listInteiros.set(i, listInteiros.get(j));
+        listInteiros.set(j, temp);
     }    
 }
-
